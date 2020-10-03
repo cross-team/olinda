e0e07b70994e23d9ee05fbbdbfef5c7b2b9c483a
Bruno Lima <bruno@cross.team>
2020-10-02 21:12:18 -0300

    Header animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import Brand from '../../images/brand.svg';
 import Navigation from '../Navigation/Navigation';
 import Button from '../Button/Button';
 
 const Header = () => {
+  const [scrolly, setScrolly] = useState('top');
+
+  const handleScroll = (position) => {
+    if (position <= -100) {
+      setScrolly('fixed');
+    } else if (position >= -100) {
+      setScrolly('top');
+    }
+  };
+
+  useScrollPosition(({ currPos }) => {
+    handleScroll(currPos.y);
+  });
+
   return (
-    <header className="header">
+    <header className={`header header--${scrolly}`}>
       <h1 className="header__brand">
         <Link to="/">
           <Brand className="header__brand__image" aria-hidden="true" />
