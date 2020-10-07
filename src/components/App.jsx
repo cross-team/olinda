4cc98ff5cc318d8e96aaa7d4b41b3b6f0ceeda77
Bruno Lima <bruno@cross.team>
2020-10-06 23:13:27 -0300

    Hero Component and Animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header/Header';
+import Hero from './Hero/Hero';
 import Footer from './Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
-      <div className="sample">Lorem ipsum</div>
+      <Hero />
       <Footer />
     </>
   );
