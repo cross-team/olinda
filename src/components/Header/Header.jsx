e3b1b264ca48380a20572367b9db50819549862e
Bruno Lima <bruno@cross.team>
2020-10-19 14:03:20 -0300

    Fixs mobile navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import { Link } from 'gatsby';
 import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import Fade from 'react-reveal/Fade';
 import Brand from '../../images/brand.svg';
-import IconMenu from '../../images/icon-menu.svg';
+import IconMenuOpen from '../../images/icon-menu.svg';
+import IconMenuClose from '../../images/icon-close.svg';
 import Navigation from '../Navigation/Navigation';
 import Button from '../Button/Button';
 
@@ -24,8 +25,18 @@ const Header = () => {
 
   const [revealed, setRevealed] = useState('hide');
 
+  const [menu, setMenu] = useState('closed');
+
+  const handleMenu = () => {
+    if (menu === 'closed') {
+      setMenu('open');
+    } else {
+      setMenu('closed');
+    }
+  };
+
   return (
-    <header className={`header header--${scrolly} header--${revealed}`}>
+    <header className={`header header--${scrolly} header--${revealed} header--${menu}`}>
       <h1 className="header__brand">
         <Fade duration={500} onReveal={() => setRevealed('revealed')}>
           <Link to="/">
@@ -34,9 +45,12 @@ const Header = () => {
           </Link>
         </Fade>
       </h1>
-      <Navigation className="header__nav" />
+      <Navigation className={`header__nav nav--${menu}`} />
       <Button className="header__contact">Contact Us</Button>
-      <IconMenu className="header__menu" />
+      <button className="header__menu" onClick={() => handleMenu()} type="button">
+        <IconMenuOpen className="header__menu__open" />
+        <IconMenuClose className="header__menu__close" />
+      </button>
     </header>
   );
 };
