5500e86b27508a62146c94dc0651ea5f7ed93de1
Bruno Lima <bruno@cross.team>
2020-10-29 12:04:35 -0300

    Fix parallax and a11y mobile menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,47 +5,47 @@ import Scrollspy from 'react-scrollspy';
 
 const Navigation = ({ className, label, ariaHidden }) => {
   return (
-    <Scrollspy
-      items={['payment-solutions', 'digital-banking', 'cards', 'about', 'support']}
-      currentClassName="nav__link--active"
-      componentTag="nav"
-      className={`nav ${className}`}
-      ariaLabel={label}
-      ariaHidden={ariaHidden}
-    >
-      <Link
-        to="/#payment-solutions"
-        className="nav__link"
-        stripHash
-        title="Navigate to Payment Solutions Section"
+    <div aria-label={label} aria-hidden={ariaHidden} className={`nav ${className}`}>
+      <Scrollspy
+        items={['payment-solutions', 'digital-banking', 'cards', 'about', 'support']}
+        currentClassName="nav__link--active"
+        componentTag="nav"
+        className="nav__wrap"
       >
-        Payment Solutions
-      </Link>
-      <Link
-        to="/#digital-banking"
-        className="nav__link"
-        stripHash
-        title="Navigate to Digital Banking section"
-      >
-        Digital Banking
-      </Link>
-      <Link to="/#cards" className="nav__link" stripHash title="Navigate to Cards Section">
-        Cards
-      </Link>
-      <Link to="/#about" className="nav__link" stripHash title="Navigate to About Us Section">
-        About Us
-      </Link>
-      <Link to="/#support" className="nav__link" stripHash title="Navigate to support Section">
-        Support
-      </Link>
-      <Link
-        to="/contact-us"
-        className="nav__link nav__link--contact"
-        title="Navigate to Contact Page"
-      >
-        Contact Us
-      </Link>
-    </Scrollspy>
+        <Link
+          to="/#payment-solutions"
+          className="nav__link"
+          stripHash
+          title="Navigate to Payment Solutions Section"
+        >
+          Payment Solutions
+        </Link>
+        <Link
+          to="/#digital-banking"
+          className="nav__link"
+          stripHash
+          title="Navigate to Digital Banking section"
+        >
+          Digital Banking
+        </Link>
+        <Link to="/#cards" className="nav__link" stripHash title="Navigate to Cards Section">
+          Cards
+        </Link>
+        <Link to="/#about" className="nav__link" stripHash title="Navigate to About Us Section">
+          About Us
+        </Link>
+        <Link to="/#support" className="nav__link" stripHash title="Navigate to support Section">
+          Support
+        </Link>
+        <Link
+          to="/contact-us"
+          className="nav__link nav__link--contact"
+          title="Navigate to Contact Page"
+        >
+          Contact Us
+        </Link>
+      </Scrollspy>
+    </div>
   );
 };
 
